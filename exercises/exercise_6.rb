4060baadee7e6dd8f936f6086b4093c9a316aecc
Junayd H <junaydhus@gmail.com>
2023-03-09 13:35:23 -0800

    update hourly wages of employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,10 +11,10 @@ puts "----------"
 # Your code goes here ...
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "John", last_name: "Adams", hourly_rate: 30)
+@store1.employees.create(first_name: "John", last_name: "Adams", hourly_rate: 40)
 @store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 45)
 
 @store2.employees.create(first_name: "Robert", last_name: "Smith", hourly_rate: 70)
-@store2.employees.create(first_name: "Jennifer", last_name: "Jackson", hourly_rate: 13)
+@store2.employees.create(first_name: "Jennifer", last_name: "Jackson", hourly_rate: 43)
 @store2.employees.create(first_name: "Joe", last_name: "Mama", hourly_rate: 50)
 
